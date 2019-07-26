fd10587e314086ce205d0fb504d279b9b8d582c9
Aadesh Salecha <salec006@umn.edu>
2019-07-26 10:52:45 -0500

    Updated IGraph Implementation
    -> Using previous hti, htw to compute current iteration
    -> Normalization at every iteration
    -> Summation starts from 0

--- igraph_TSM.py
+++ igraph_TSM.py
@@ -1,114 +1,107 @@
-from igraph import *
 import re
 import math
 import csv
+import copy
+import pickle
+import igraph
+
+inv = 0.359
+iter_count = 20
+normChoice = 0
+
+def TSM(graph, vertices, cur_dir = './'):
+  print('Calculating TSM scores.')
+
+  hti = {}
+  htw = {}
+
+  for node in vertices:
+    hti[node] = 1 #/float(len(vertices))
+    htw[node] = 1 #/float(len(vertices))
+
+  i = 0
+  while(i < iter_count):
+    print('Iteration ', i+1, ' in progress...')
+    
+    prev_hti = copy.deepcopy(hti); prev_htw = copy.deepcopy(htw)
+
+    for node in vertices:
+      """Calculate Scores for Trustingness"""
+      vsti = graph.neighbors(node, mode='OUT')
+      sc = calcScores(graph, vsti, 0, node, prev_htw, 'ti')
+      hti[node] = sc
+
+    for node in vertices:
+      """Calculate Scores for Trustworthiness"""
+      vstw = graph.neighbors(node, mode='IN')
+      sc = calcScores(graph, vstw, 0, node, prev_hti, 'tw')
+      htw[node] = sc
+
+    hti = normalize(hti, normChoice)
+    htw = normalize(htw, normChoice)
+    i += 1
+  
+  TSM_scores = {}
+  for node in vertices:
+    TSM_scores[node] = (hti[node], htw[node])
+  pickle.dump(TSM_scores, open(cur_dir + '/Concatenated/TSM_scores', 'wb'))
 
 
-inv = 1
-iter_count = 10
-delimiter = '\t'
-normChoice = 1
+def calcScores(G, vs, s, n, other_sc, flag):
+  ########################################################
+  ############## ASSUMES WEIGHT = 1 ALWAYS ###############
+  ########################################################
 
-input_file = 'data/soc-Epinions1_ip.txt'
-output_file = 'results/soc-Epinions1_ip_TS.txt'
-
-g = Graph(directed=True)
-vertices_set = set()
-edges = dict()
-f = open(input_file, 'r')
-lines = f.readlines()
-for line in lines:
-    l_spl = re.split(delimiter, line.rstrip())
-    l = []
-    vertices_set.add(int(l_spl[0]))
-    l.append(int(l_spl[0]))
-    vertices_set.add(int(l_spl[1]))
-    l.append(int(l_spl[1]))
-    if len(l_spl) == 2:  # Un-weighted graph
-        edges[tuple(l)] = 1
-    else:  # Weighted graph
-        edges[tuple(l)] = int(l_spl[2])
-
-
-vertices = list(vertices_set)
-
-g = Graph(vertex_attrs={"label": vertices}, edges=edges.keys(), directed=True)
-print 'Graph generated. Calculating scores...'
-
-hti = {}
-htw = {}
-
-for node in vertices:
-    hti[node] = 1 /float(len(vertices))
-    htw[node] = 1 /float(len(vertices))
-
-
-def calcScores(vs, s, n, other_sc, flag):
-    if flag == 'ti':
-        for vertex in vs:
-            s += inver(other_sc.get(vertex))*edges[(n, vertex)]
-    elif flag == 'tw':
-        for vertex in vs:
-            s += inver(other_sc.get(vertex))*edges[(vertex, n)]
-    return s
-
+  s = 0
+  if flag == 'ti':
+    for vertex in vs:
+      s += inver(other_sc.get(vertex))*1 #edges[(n, vertex)]
+  elif flag == 'tw':
+    for vertex in vs:
+      s += inver(other_sc.get(vertex))*1 #edges[(vertex, n)]
+  return s
 
 def inver(a):
-    return 1/float(1+a**inv)
-
+  return 1/float(1+a**inv)
 
 def normalize(userScores, choice):
-    score_list = userScores.values()
+  score_list = userScores.values()
+  if choice == 0:  # min-max
     min_val = min(score_list)
     max_val = max(score_list)
-    if choice == 0:  # min-max
-        for user in userScores:
-            userScores[user] = (userScores[user] - min_val)/float(max_val - min_val)
+    for user in userScores:
+      userScores[user] = (userScores[user] - min_val)/float(max_val - min_val)
 
+  elif choice == 1: # sum-of-squares
+    norm_den = sum(i**2 for i in score_list)
+    norm_den = math.sqrt(norm_den)
+    for user in userScores:
+      userScores[user] = userScores[user]/float(norm_den)
 
-    elif choice == 1: # sum-of-squares
-        norm_den = sum(i**2 for i in score_list)
-        norm_den = math.sqrt(norm_den)
-        for user in userScores:
-            userScores[user] = userScores[user]/float(norm_den)
+  return userScores
 
-    return userScores
+if __name__ == '__main__':
+  # G = igraph.Graph([(1, 3), (4, 1), (2, 3), (3, 5), (2, 5)], directed = True)
+  # TSM('sample', G, [1, 2, 3, 4, 5])
 
+  edges = [(1, 0), (2, 0), (3, 0), (4, 0)]
+  edges += [(5, 1), (6, 1), (7, 2), (8, 2), (9, 2), (10, 3), (11, 3), (12, 3), (13, 4), (14, 4), (15, 4)]
+  # edges += [(16, 11), (17, 12), (17, 13), (18, 14)]
+  G = igraph.Graph(edges, directed = True)
+  TSM('sample', G, list(range(0, 16)))
 
+  # edges = {}
+  # vertices = set()
+  # with open(input_file, 'r') as inptr:
+  #   for line in inptr:
+  #     l_spl = re.split(',', line.rstrip())
+  #     if len(l_spl) == 2:
+  #       try:
+  #         edges[tuple(id_dict[l_spl[0]], id_dict[l_spl[1]])] = 1
+  #         vertices.add(id_dict(l_spl[0]))
+  #         vertices.add(id_dict(l_spl[1]))
+  #       except KeyError:
+  #         continue
 
-i = 0
-while(i < iter_count):
-    print 'Iteration ', i+1, ' in progress...'
-    for node in vertices:
-            """Calculate Scores for Trustingness"""
-            vsti = g.neighbors(node, mode=OUT)
-            sc = calcScores(vsti, hti.get(node), node, htw, 'ti')
-            hti[node] = sc
-
-    for node in vertices:
-            """Calculate Scores for Trustworthiness"""
-            vstw = g.neighbors(node, mode=IN)
-            sc = calcScores(vstw, htw.get(node), node, hti, 'tw')
-            htw[node] = sc
-
-    i += 1
-
-norm_hti = normalize(hti, normChoice)
-norm_htw = normalize(htw, normChoice)
-
-with open(output_file, 'w') as f:
-    writer = csv.writer(f)
-    for i in vertices:
-        print i, ' , ',norm_hti[i], ' , ' ,norm_htw[i]
-        l = []
-        l.append(i)
-        l.append(norm_hti[i])
-        l.append(norm_htw[i])
-        writer.writerow(l)
-
-
-
-
-
-
-
+  # G = igraph.Graph(edges.keys(), directed = True)
+  # TSM('sample', G = G, vertices = list(vertices))
